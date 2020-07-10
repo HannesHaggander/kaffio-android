7bdcc05df552c203b932d6ddbeeec1f73cf56f49
Hannes <h.haggander@gmail.com>
2020-07-10 23:31:53 +0200

    add tests for inserting constraints to events

--- app/src/main/java/com/towerowl/kaffio/data/CoffeeEvent.kt
+++ app/src/main/java/com/towerowl/kaffio/data/CoffeeEvent.kt
@@ -3,17 +3,17 @@ package com.towerowl.kaffio.data
 import java.util.*
 
 data class CoffeeEvent(
-    val id: UUID = UUID.randomUUID()
+    val id: UUID = UUID.randomUUID(),
+    var name: String,
+    var groupLimit: Int = -1
 ) {
-    private val mMembers: MutableList<User> = mutableListOf()
-    val members: List<User> get() = mMembers
+    private val mMembers = mutableListOf<User>()
+    val members: List<User> get() = mMembers.toList()
 
-    fun addMember(user: User) {
+    fun addMember(user: User): Boolean =
+        if (groupLimit >= 0 && members.size >= groupLimit || mMembers.contains(user)) false
+        else mMembers.add(user)
 
-        mMembers.add(user)
-    }
+    fun removeMember(user: User) = mMembers.remove(user)
 
-    fun removeMember(user: User) {
-        mMembers.remove(user)
-    }
 }
--- app/src/test/java/com/towerowl/kaffio/UnitTests.kt
+++ app/src/test/java/com/towerowl/kaffio/UnitTests.kt
@@ -24,14 +24,14 @@ class UnitTests {
 //
 //    }
 
-    //- verify changes to event
+    //- verify safe handling of group limit reduction when more members are in
 
     /**
      * Check that users added to events are in the interest list
      */
     @Test
-    fun `Check that users are added and removed from events`(){
-        val event = CoffeeEvent()
+    fun `Check that users are added and removed from events`() {
+        val event = CoffeeEvent(name = "test event")
         val userA = User(name = "Name a")
         event.addMember(userA)
         assert(event.members.contains(userA))
@@ -39,7 +39,30 @@ class UnitTests {
         assert(!event.members.contains(userA))
     }
 
-    //- Verify that users cant join a full event
+    /**
+     * Verify that users cant join a full event
+     */
+    @Test
+    fun `Verify user can not join a full event`() {
+        val limit = 5
+        CoffeeEvent(name = "test event", groupLimit = limit - 1).run {
+            for (i in 0 until limit) {
+                addMember(User(name = "user:$i"))
+            }
+
+            assert(members.size < limit)
+        }
+    }
+
+    @Test
+    fun `Verify no duplicates users in event`(){
+        val event = CoffeeEvent(name = "test event")
+        User(name = "test user").run {
+            event.addMember(this)
+            event.addMember(this)
+        }
+        assert(event.members.size == 1)
+    }
 
 
     //- control that only the creator and admin can change an event
